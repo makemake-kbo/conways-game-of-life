3cdd204042988bf19fd54107ba1522a6646fdc88
vgostovic <vukasin@gostovic.de>
2020-12-01 14:12:07 +0100

    Added displayGrid function.

--- game-of-life/evaluation.cs
+++ game-of-life/evaluation.cs
@@ -8,22 +8,31 @@ namespace gameoflife
 			for (int x = 0; x < grid.GetLength(0); x++) {
 				for (int y = 0; y < grid.GetLength(1); y++) {
 					int count = grid[x, y]
-              					+ grid[x, y+1]
-              					+ grid[x+1, y]
-              					+ grid[x+1, y+1];
-              		if (!((x-1) < 0)) {
-              			count = count + grid[x-1, y]
-              					+ grid[x-1, y+1];
-              		}
-              		if (!((y-1) < 0)) {
-              			count = count + grid[x, y-1]
-              					+ grid[x+1, y-1];
-              		}
-              		if (!((y-1) < 0) && !((x-1) < 0)) {
-              			count = count + grid[x-1,y-1];
-              		}
-              		grid[x, y] = isAlive (grid[x, y], count);
+								+ grid[x, y+1]
+								+ grid[x+1, y]
+								+ grid[x+1, y+1];
+					if (!((x-1) < 0)) {
+						count = count + grid[x-1, y]
+									+ grid[x-1, y+1];
+					}
+					if (!((y-1) < 0)) {
+						count = count + grid[x, y-1]
+									+ grid[x+1, y-1];
+					}
+					if (!((y-1) < 0) && !((x-1) < 0)) {
+						count = count + grid[x-1,y-1];
+					}
+					grid[x, y] = isAlive (grid[x, y], count);
 				}
+			}
+		}
+
+		public void displayGrid(int[,] grid) {
+			for (int x = 0; x < grid.GetLength(0); x++) {
+				for (int y = 0; y < grid.GetLength(1); y++) {
+					Console.Write(string.Format("{0} ", grid[x, y]));
+				}
+				Console.Write(Environment.NewLine + Environment.NewLine);
 			}
 		}
 	}
