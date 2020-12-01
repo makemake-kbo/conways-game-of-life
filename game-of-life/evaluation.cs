fe29b038ad90e1cbff212e9270705cd3e9402d9c
vgostovic <vukasin@gostovic.de>
2020-12-01 13:48:49 +0100

    added conditions for when the array can go out-of-bounds

--- game-of-life/evaluation.cs
+++ game-of-life/evaluation.cs
@@ -7,16 +7,23 @@ namespace gameoflife
 		public void evaluateGrid(int[,] grid){
 			for (int x = 0; x < grid.GetLength(0); x++) {
 				for (int y = 0; y < grid.GetLength(1); y++) {
-					int count = grid[x-1,y-1] 
-              					+ grid[x-1,y  ] 
-              					+ grid[x-1,y+1] 
-              					+ grid[x  ,y-1] 
-              					+ grid[x  ,y  ] 
-              					+ grid[x  ,y+1] 
-              					+ grid[x+1,y-1] 
-              					+ grid[x+1,y  ] 
-              					+ grid[x+1,y+1];
-
+					int count = grid[x, y]
+              					+ grid[x, y+1]
+              					+ grid[x+1, y]
+              					+ grid[x+1, y+1];
+              		if (!((x-1) < 0)) 
+              		{
+              			count = count + grid[x-1, y]
+              					+ grid[x-1, y+1];
+              		}
+              		if (!((y-1) < 0))  
+              		{
+              			count = count + grid[x, y-1]
+              					+ grid[x+1, y-1];
+              		}
+              		if (!((y-1) < 0) && !((x-1) < 0)) {
+              			count = count + grid[x-1,y-1];
+              		}
 				}
 			}
 		}
